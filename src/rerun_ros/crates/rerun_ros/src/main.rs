c4026d44a6f35596f58b55a76f4d0fc06e54dc0b
Christian Johansen <johansen.christian.a@gmail.com>
2025-08-13 16:35:55 +0000

    Test using explicit colcon ws and generating std_msgs

--- src/rerun_ros/crates/rerun_ros/src/main.rs
+++ src/rerun_ros/crates/rerun_ros/src/main.rs
@@ -1,6 +1,6 @@
 use anyhow::{Error, Result};
 use clap::Parser;
-use rclrs::CreateBasicExecutor;
+use rclrs::{CreateBasicExecutor, InitOptions};
 use rerun_ros::config::ConfigParser;
 use std::env;
 use std::sync::Arc;
@@ -24,7 +24,7 @@ fn main() -> Result<(), Error> {
     println!("Starting bridge");
     let config_parser = ConfigParser::new(&bridge_args.config_file)?;
 
-    let context = rclrs::Context::new(env::args())?;
+    let context = rclrs::Context::new(env::args(), InitOptions::new())?;
     let executor = context.create_basic_executor();
     let node = executor.create_node("rerun_ros_bridge")?;
     // Clippy does not like iterating over the keys of a HashMap, so we collect it into a Vec
