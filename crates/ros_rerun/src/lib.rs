785bd3e0a313be9bf8fa6d2e3b5d73505f05b043
Christian Johansen <johansen.christian.a@gmail.com>
2025-11-04 09:58:10 -0800

    feat: add basic top-level docs

--- crates/ros_rerun/src/lib.rs
+++ crates/ros_rerun/src/lib.rs
@@ -1,3 +1,11 @@
+//! ROS <-> Rerun integration
+//! This crate provides functionality to connect ROS2 to Rerun,
+//! primarily for visualization of robot data using the Rerun viewer.
+//!
+//! The primary use case for the `ros_rerun` bridge is to provide a binary
+//! that can be installed on a ROS2 system to stream data to Rerun viewer(s),
+//! often over a network, or to record data locally for later playback.
+
 pub mod archetypes;
 pub mod ros_introspection;
 
